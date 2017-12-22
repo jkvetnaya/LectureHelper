bbdd56f3e748e4e84bb44c413c0f887952379145
jkvetnaya <jkvetnaya@users.noreply.github.com>
2017-12-21 22:29:47 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+#directory where coursera materials located
+courseradir = ""
+
+#output directory
+lecturedir = ""
+
+#directory where ffmpeg executable is located
+ffmpegdir = ""
+ffmpeg = ffmpegdir + "/ffmpeg"
--- lecture.py
+++ lecture.py
@@ -2,32 +2,26 @@ import textrank
 import click
 import scrapy
 from attr._make import fields
+import os
 
-filename = "/Users/julia/UIUC/CS410/Project/Lessons/Week3/Lesson3.3/subtitles.vtt"
-fileout = "/Users/julia/UIUC/CS410/Project/CS410mat/Week3/Lesson3.3.html"
-imgdir = "/Users/julia/UIUC/CS410/Project/Slides/Week3/Lesson3.3/"
-
-ignore_list = "WEBVTT, [MUSIC],[INAUDIBLE]"
+ignore_list = "WEBVTT, [MUSIC],[INAUDIBLE], [SOUND], >>"
 stack = ""
 
 curminute = "00:00:00.000"
 curtext = []
 newMinute = True
-fout = open(fileout,"w+")
 
 
 
-def toHTML(text):
-    global fout
-    fout.write("<p>")
-    fout.write(text)
-    fout.write("</p>")
-    fout.write("\n")
+def toHTML(fhtml, text):
+    fhtml.write("<p>")
+    fhtml.write(text)
+    fhtml.write("</p>")
+    fhtml.write("\n")
 
-def toHTML2(text):
-    global fout
-    fout.write(text)
-    fout.write("\n")
+def toHTML2(fhtml, text):
+    fhtml.write(text)
+    fhtml.write("\n")
     
 def getmin(timestamp):
     fields = splitTS(timestamp)
@@ -67,8 +61,8 @@ def istimestampline(words):
             return True
     return False
 
-def process_line(line):    
-    global curminute
+def process_line(fhtml, imgdir, line):    
+    global curminute 
     global curtext
     global newMinute
     if len(line) > 0:
@@ -76,37 +70,140 @@ def process_line(line):
         if not ifcounter(words):
             if istimestampline(words):
                 if issameminute(curminute, words[0]) == False:
-                    toHTML(curminute)
+                    toHTML(fhtml, curminute)
                     curminute = words[0]
                     newMinute = True                    
             else:
                 if newMinute == True:
                     newMinute = False
-                    toHTML(" ".join(curtext))
-                    imgcommand = "<img src=\"" + imgdir + "img0" + getmin(curminute) + ".jpg\"" + ">"
-                    toHTML2(imgcommand)
+                    toHTML(fhtml, " ".join(curtext))
+                    imgcommand = "<img src=\"" + imgdir + "/img0" + getmin(curminute) + ".jpg\"" + ">"
+                    toHTML2(fhtml, imgcommand)
                     del curtext[:]
                 for word in words: 
                     if word not in ignore_list:
                         curtext.append(word)
 
 
-message = """<html>
-<head>Lesson 3.3</head>
-<body>"""
+def createLecture(c_radir, htmldir, imgdir, lname):
+    global curminute
+    global curtext 
+    global newMinute 
+    
+    curminute = "00:00:00.000"
+    curtext = []
+    newMinute = True
+    
+    message = """<html>
+    <head><h2>"""
+    message += lname
+    message += """</h2></head>
+    <body>"""
+    
+    htmlfile = htmldir + "/" + lname + ".html"
+    fhtml = open(htmlfile,"w+")
+    fhtml.write(message)
+    
+    c_ravtt = c_radir + "/subtitles.vtt"
 
-fout.write(message)
-
-
-
-with open(filename) as f:
-    for line in f:
-        line = line.rstrip()
-        process_line(line)
+    with open(c_ravtt) as f:
+        for line in f:
+            line = line.rstrip()
+            process_line(fhtml, imgdir, line)
   
-message = """
-</body>
-</html>"""  
-fout.close()
+        message = """
+        </body>
+        </html>"""  
+        fhtml.close()
+
+
+
+def makeImageDir(lname):
+    tmpname = lecturedir
+    if os.path.exists(tmpname) == False:
+        os.mkdir(tmpname)
+    tmpname += "/images"
+    if os.path.exists(tmpname) == False:
+        os.mkdir(tmpname)
+    tmpname += "/" + lname
+    if os.path.exists(tmpname) == False:
+        os.mkdir(tmpname)
+    return tmpname
+
+def makeHtmlDir():   
+    tmpname = lecturedir
+    if os.path.exists(tmpname) == False:
+        os.mkdir(tmpname)
+    tmpname += "/lectures"
+    if os.path.exists(tmpname) == False:
+        os.mkdir(tmpname)
+    return tmpname
+    
+        
+def callFfmpeg(c_radir, imgdir):
+    cmd = ffmpeg + " -i " + c_radir + "/index.mp4 -vf fps=1/60 " + imgdir + "/img%03d.jpg"
+    var = os.system(cmd)
+    
+def kwToString(keywords):
+    lk = list(keywords)
+    return ", ".join(lk)
+
+def firstSentence(txttext):
+    import re
+    sentences = re.split('\. |: |;', txttext)
+    return sentences[0] + "."
+
+def cleanText(txttext):
+    return ' '.join(filter(lambda x: x not in ignore_list,  txttext.split()))
+
+def __main__ ():
+    courseras = os.listdir(courseradir)
+    
+    hdir = makeHtmlDir()
+    
+    rootHtmlName = hdir + "/Lectures.html"
+    
+    message = """<html>
+    <head><h2>Lectures</h2></head>
+    <body>"""
+    
+    fhtml = open(rootHtmlName,"w+")
+    fhtml.write(message)
+
+    for d in courseras:
+        if(d[0] != "."):
+            print "Processing " + d
+            idir = makeImageDir(d)
+            callFfmpeg(courseradir + "/" + d, idir)           
+            createLecture(courseradir + "/" + d , hdir, idir, d)
+            toHTML(fhtml, "<a href=" + hdir +"/" + d + ".html>" + d + "</a>" )
+            txtfile = courseradir + "/" + d + "/subtitle.txt"
+            with open(txtfile) as f:
+                txttext = f.read()
+                txttext = cleanText(txttext)
+                fs = firstSentence(txttext)
+                toHTML(fhtml, "<b>" + fs + "</b>")
+                print "Creating summary for " + d
+                summary = textrank.extract_sentences(txttext)
+                toHTML(fhtml, "<u>Summary:</u>")
+                toHTML(fhtml, summary)
+                print "Collecting keywords " + d
+                keywords = textrank.extract_key_phrases(txttext)                
+                toHTML(fhtml, "<u>Key-phrases:</u>")
+                toHTML(fhtml, kwToString(keywords))
+    message = """
+    </body>
+    </html>"""  
+    fhtml.close()
+
+from config import courseradir
+from config import lecturedir 
+from config import ffmpegdir
+from config import ffmpeg
+
+if courseradir == "" or  lecturedir == "" or ffmpegdir == "":
+	print "Improper configuration. Fix config.py"
+else : 
+	__main__()
 
 print "All done"
